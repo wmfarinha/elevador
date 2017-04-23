7a0d500b42100baff19c5cb167e89256a46bdaf1
William Faria <wmfarinha@gmail.com>
2017-04-23 14:19:14 -0300

    Funcionamento básico do elevador

--- js/controllers.js
+++ js/controllers.js
@@ -2,30 +2,67 @@ function MainCtrl($scope) {
     var vm = this;
 }
 
-function ElevadorCtrl($scope){
+function ElevadorCtrl($scope, $filter) {
     var vm = this;
 
-    vm.andar = [0,1,2,3];
+    vm.qtdAndar = 6;
     vm.passageiros = [];
     vm.qtdPassageiros = 0;
     vm.statusElevador = "Porta Aberta";
     vm.andarElevador = 0;
 
-    function adicionaPassageiro(passageiro){
+    function adicionaPassageiro(passageiro) {
         vm.passageiros.push({
-                andar: 0,
-                passageiro: passageiro
-            });
+            andar: 0,
+            passageiro: passageiro
+        });
     }
 
-    vm.adicionaQtdPassageiros = function() {
-        for(let i = 0; i < vm.qtdPassageiros; i++){
+    vm.adicionaQtdPassageiros = function () {
+        for (let i = 0; i < vm.qtdPassageiros; i++) {
             adicionaPassageiro(i);
         };
     }
 
-    function iniciaRota(){
+    function sobe() {
 
+        console.log('Fecha Porta')
+
+        for (let i = 1; i <= vm.qtdAndar; i++) {
+
+            vm.andarElevador = i;
+
+            console.log('Vai para Andar -> ' + vm.andarElevador);
+
+            //verfica que se tem passageiros para descer
+            let passageiroDesce = $filter('filter')(vm.passageiros, {
+                andar: vm.andarElevador
+            });
+
+            if (passageiroDesce != null && passageiroDesce.length > 0) {
+                console.log('Abre a porta');
+
+                for (let j = 0; j < passageiroDesce.length; j++) {
+                    vm.passageiros.splice(vm.passageiros.indexOf(passageiroDesce[j]), 1);
+                    console.log('Sai passageiro -> ' + passageiroDesce[j].passageiro);
+                }
+
+                console.log('Fecha porta');
+
+            } else {
+                console.log('Não tem passageiros para descer no andar ' + vm.andarElevador);
+            }
+        }
+    }
+
+    function desce() {
+        for (let i = vm.qtdAndar; i <= vm.qtdAndar; i--) {
+
+        }
+    }
+
+    vm.fecharPorta = function () {
+        sobe();
     }
 }
 
